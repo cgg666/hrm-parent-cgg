98d894f58a2a4b56166cc2e44a75f29a1d6eeb60
cgg666 <1002022813@qq.com>
2020-06-18 21:39:13 +0800

    代码生成器

---
+++ hrm-basic-parent/hrm-basic-boot-starter/src/main/java/cuiyi/itsource/hrm/autoconfig/SwaggerAutoConfiguration.java
@@ -0,0 +1,49 @@
+package cuiyi.itsource.hrm.autoconfig;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableConfigurationProperties(SwaggerProperties.class)
+public class SwaggerAutoConfiguration {
+
+    private SwaggerProperties swaggerProperties;
+
+    public SwaggerAutoConfiguration(SwaggerProperties swaggerProperties){
+        this.swaggerProperties = swaggerProperties;
+    }
+
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //对外暴露服务的包,以controller的方式暴露,所以就是controller的包.
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getPackageName()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .contact(new Contact(swaggerProperties.getName(), swaggerProperties.getUrl(), swaggerProperties.getEmail()))
+                .version(swaggerProperties.getVersion())
+                .build();
+    }
+
+}
--- hrm-support-parent/hrm-zuul-server/src/main/java/cuiyi/itsource/hrm/config/SwaggerConfig.java
+++ hrm-support-parent/hrm-zuul-server/src/main/java/cuiyi/itsource/hrm/config/SwaggerConfig.java
@@ -3,8 +3,6 @@ package cuiyi.itsource.hrm.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -13,24 +11,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2 {
+public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                //对外暴露服务的包,以controller的方式暴露,所以就是controller的包.
-                .apis(RequestHandlerSelectors.basePackage("cuiyi.itsource.hrm.controller"))
-                .paths(PathSelectors.any())
-                .build();
+                .apiInfo(apiInfo());
     }
-
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("系统服务api")
-                .description("系统服务接口文档说明")
-                .contact(new Contact("cuiyi", "", "cuiyi@itsource.cn"))
+                .title("分布式购物系统")
+                .description("购物系统接口文档说明")
+                .termsOfServiceUrl("http://localhost:8081")
+                .contact(new Contact("yphtest", "", "yhp@itsoruce.cn"))
                 .version("1.0")
                 .build();
     }
