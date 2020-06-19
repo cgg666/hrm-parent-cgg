b53954af44d4455bf891e6209f07b31267df3736
cgg666 <1002022813@qq.com>
2020-06-19 08:55:07 +0800

    解决跨域等问题

--- hrm-system-parent/hrm-system-service/src/main/java/cuiyi/itsource/hrm/controller/EmployeeController.java
+++ hrm-system-parent/hrm-system-service/src/main/java/cuiyi/itsource/hrm/controller/EmployeeController.java
@@ -1,18 +1,18 @@
 package cuiyi.itsource.hrm.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import cuiyi.itsource.hrm.domain.Employee;
 import cuiyi.itsource.hrm.service.IEmployeeService;
 import cuiyi.itsource.hrm.util.AjaxResult;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class EmployeeController {
+
     @Autowired
     private IEmployeeService employeeService;
 
@@ -41,4 +41,5 @@ public class EmployeeController {
         return AjaxResult.me().setSuccess(true).setMessage("登录成功!").setResultObj(loginUser);
 
     }
+
 }
