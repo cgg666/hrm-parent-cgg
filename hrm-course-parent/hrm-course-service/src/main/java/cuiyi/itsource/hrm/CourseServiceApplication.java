7538afb79db1658c0a0f59942b706f51760385fe
cgg666 <1002022813@qq.com>
2020-06-22 20:25:48 +0800

    课程维护，代码生成

--- hrm-course-parent/hrm-course-service/src/main/java/cuiyi/itsource/hrm/CourseServiceApplication.java
+++ hrm-course-parent/hrm-course-service/src/main/java/cuiyi/itsource/hrm/CourseServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@MapperScan("cn.itsource.hrm.mapper")
+@MapperScan("cuiyi.itsource.hrm.mapper")
 @EnableFeignClients //开启feign
 public class CourseServiceApplication {
 
